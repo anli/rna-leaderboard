b9f5475c6d9211e11f1f9a7e4e5adcd24f7663bb
Su Anli <sual0001@gmail.com>
2020-04-03 11:30:55 +0800

    chore(package, src, app, tsconfig): add react-navigation lib, refactor to screens, path alias config

---
+++ src/screens/home/home.tsx
@@ -0,0 +1,64 @@
+import firestore from '@react-native-firebase/firestore';
+import React, {useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
+
+const HomeScreen = () => {
+  const {data} = useApp();
+
+  return (
+    <>
+      <View>
+        <Text>Test</Text>
+        {data?.map(({id, title, winner, date, participants}) => (
+          <View key={id}>
+            <Text>{title}</Text>
+            <Text>{winner}</Text>
+            <Text>{date}</Text>
+            {participants?.map(participant => (
+              <Text key={participant}>{participant}</Text>
+            ))}
+          </View>
+        ))}
+      </View>
+    </>
+  );
+};
+
+export default HomeScreen;
+
+interface Play {
+  id: string;
+  title: string;
+  winner: string;
+  date: string;
+  participants: string[];
+}
+
+const useApp = () => {
+  const [data, setData] = useState<Play[] | undefined>(undefined);
+
+  useEffect(() => {
+    const unsubscribe = firestore()
+      .collection('plays')
+      .onSnapshot(querySnapshot => {
+        const mappedData = querySnapshot.docs.map(documentSnapshot => {
+          const record: Play = {
+            title: documentSnapshot.data().title,
+            id: documentSnapshot.id,
+            winner: documentSnapshot.data().winner,
+            date: documentSnapshot.data().date,
+            participants: documentSnapshot.data().participants,
+          };
+
+          return record;
+        });
+
+        setData(mappedData);
+      });
+
+    /* istanbul ignore next */
+    return () => unsubscribe();
+  }, []);
+
+  return {data};
+};
