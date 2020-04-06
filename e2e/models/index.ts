2c57ba277bc1dce4abb1dc3b80b6ade140dfa841
Su Anli <sual0001@gmail.com>
2020-04-06 09:16:21 +0800

    test(e2e/models): remove unneeded waitFor in button tap

--- e2e/models/index.ts
+++ e2e/models/index.ts
@@ -3,18 +3,12 @@ import {by, element, expect, waitFor} from 'detox';
 export class PlayDetailScreen {
   static iPressDeleteButton$ = async () => {
     const button = element(by.id('play-delete-button'));
-    await waitFor(button)
-      .toBeVisible()
-      .withTimeout(10000);
     await button.tap();
     return;
   };
 
   static iPressDeleteConfirmButton$ = async () => {
     const button = element(by.text('Confirm'));
-    await waitFor(button)
-      .toBeVisible()
-      .withTimeout(10000);
     await button.tap();
     return;
   };
@@ -23,9 +17,6 @@ export class PlayDetailScreen {
 export class HomeScreen {
   static iAmAtPlayDetailScreen$ = async () => {
     const button = element(by.id('play-detail-button-0'));
-    await waitFor(button)
-      .toBeVisible()
-      .withTimeout(10000);
     await button.tap();
 
     const playDetailScreenId = by.id('play-detail-screen');
